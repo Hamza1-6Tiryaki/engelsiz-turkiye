743bf6c1a87e5c6203082ffa77ba3cd6d3ded430
Erisilebilir Turkiye <destek@erisimturkiye.com>
2026-05-10 03:30:47 +0300

    Fix: main.dart dosyasindaki yanlis import satiri silinerek derleme hatasi giderildi

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'presentation/pages/daily_life_page.dart';
 import 'presentation/pages/profile_page.dart';
 import 'presentation/pages/admin_panel_page.dart';
 import 'presentation/pages/talkback_main_page.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
